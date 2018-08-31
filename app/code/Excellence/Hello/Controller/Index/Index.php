b0ae337a8b03f0270e25d47e08e016f90e23f2db
Vivek Singh <vivek.singh@cardinaltsinc.net>
2018-08-31 22:28:32 +0530

    initial commit

---
+++ app/code/Excellence/Hello/Controller/Index/Index.php
@@ -0,0 +1,18 @@
+<?php
+namespace Excellence\Hello\Controller\Index;
+ 
+ 
+class Index extends \Magento\Framework\App\Action\Action
+{
+    public function __construct(
+        \Magento\Framework\App\Action\Context $context)
+    {
+        return parent::__construct($context);
+    }
+     
+    public function execute()
+    {
+        echo 'Home...!';
+        exit;
+    } 
+}
